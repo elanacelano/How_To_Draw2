d14a326c44b7471e92ff9321414fab0061f81023
Elana Celano <elanacelano@gmail.com>
2017-09-02 14:49:35 -0400

    can't figure out why it's not working

--- build/bundle.js
+++ build/bundle.js
@@ -32324,7 +32324,6 @@ function getTransitionProperties() {
 /* 241 */
 /***/ (function(module, exports) {
 
-"use strict";
 throw new Error("Module build failed: SyntaxError: Unexpected token (7:0)\n\n\u001b[0m \u001b[90m  5 | \u001b[39m\n \u001b[90m  6 | \u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m  7 | \u001b[39m\u001b[36mdefault\u001b[39m \u001b[36mclass\u001b[39m \u001b[33mLogin\u001b[39m \u001b[36mextends\u001b[39m \u001b[33mReact\u001b[39m\u001b[33m.\u001b[39m\u001b[33mComponent\u001b[39m {\n \u001b[90m    | \u001b[39m\u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m  8 | \u001b[39m  constructor(props){\n \u001b[90m  9 | \u001b[39m    \u001b[36msuper\u001b[39m(props)\u001b[33m;\u001b[39m\n \u001b[90m 10 | \u001b[39m    \u001b[36mthis\u001b[39m\u001b[33m.\u001b[39mstate \u001b[33m=\u001b[39m {\u001b[0m\n");
 
 /***/ })
