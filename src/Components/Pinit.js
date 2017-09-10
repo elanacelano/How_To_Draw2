23e81ed441c47461d9bcbe9c33d1327cfe6ab73c
Elana Celano <elanacelano@gmail.com>
2017-09-10 14:10:58 -0400

    a little more on piterest

--- src/Components/Pinit.js
+++ src/Components/Pinit.js
@@ -22,30 +22,15 @@ export default class Pinit extends React.Component {
 			    async defer
 			    src="//assets.pinterest.com/js/pinit.js">
 				<a href="https://www.pinterest.com/pin/567735096748540707/"data-pin-do="embedPin" width="100%"></a>
-
-
-
-
     		
         		<div key="4922014937288551472"><img src="https://www.pinterest.com/pin/567735096748540707/"></img></div>
-        			<span style="z-index: 2; opacity: 1; display: block; position: absolute; top: 0px; left: 0px; width: 150px; transition: opacity 480ms cubic-bezier(0.165, 0.84, 0.44, 1), transform 480ms cubic-bezier(0.165, 0.84, 0.44, 1); transform: translateX(186.5px) translateY(87px);"><div class="item item--pattern2" style="height: 217px;"></div></span>
-
-        		<div key="4922014937288551472"><img src="https://www.pinterest.com/pin/487233253428680844/"></img></div>
-        		<div key="4922014937288551472"><img src="https://www.pinterest.com/pin/193795590184950367/"></img></div>        		        		
-        		
+        			
+        		</script>
     		</StackGrid>
     		<p>PICTURES</p>
     		
-			  
-				<a href="https://www.pinterest.com/pin/567735096748540707/"data-pin-do="embedPin"width="100%"height="auto"key="4922014937288551472"></a>
-    		
-        		<div key="4922014937288551472"><img src="https://www.pinterest.com/pin/193795590184950367/data-pin-do="embedPin""></img>
-        			
-        		<div key="4922014937288551472"><img src="https://www.pinterest.com/pin/487233253428680844/"></img>
-        		<div key="4922014937288551472"><img src="https://www.pinterest.com/pin/193795590184950367/"></img>   
-        	</StackGrid>
-    		<p>PICTURES</p>
 
+    		
 		</div>
         );
       }
