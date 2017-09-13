fbaefe247d06f0f49e051932b748c880408d6a7f
Elana Celano <elanacelano@gmail.com>
2017-09-12 21:21:06 -0400

    more changes to heroku

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 var session = require("express-session");
-var User = require("./models/User");
 var app = express();
 var cheerio = require("cheerio");
 
@@ -25,53 +24,7 @@ app.get("/", function(req, res) {
 })
 
 
-app.post("/create/user", function(req, res){
-	console.log(req.body);
-	User.create({
-		username:req.body.email,
-		password:req.body.password
-	}, function(err,data){
-		if(err) throw err;
-		console.log(data);
-		return data;
-	})
-});
-// Scrape data from one site and place it into the mongodb db
-app.get("/scrape", function(req, res) {
-  // Make a request for the news section of ycombinator
-  request("https://news.ycombinator.com/", function(error, response, html) {
-    // Load the html body from request into cheerio
-    var $ = cheerio.load(html);
-    // For each element with a "title" class
-    $(".title").each(function(i, element) {
-      // Save the text and href of each link enclosed in the current element
-      var title = $(element).children("a").text();
-      var link = $(element).children("a").attr("href");
 
-      // If this found element had both a title and a link
-      if (title && link) {
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title: title,
-          link: link
-        },
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log(inserted);
-          }
-        });
-      }
-    });
-  });
-
-  // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
-});
 
 
 app.get("/DrawingVideos", function(req, res) {
@@ -112,8 +65,8 @@ app.post("*", function(req, res){
 
 var port = process.env.PORT || 3000;
 
-app.listen(PORT, function(){
-  console.log("App listening on PORT: " + PORT);
+app.listen(port, function(){
+  console.log("App listening on PORT: " + port);
 });
 
 
