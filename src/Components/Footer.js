af5c612bd9ee00f97db432f13602423db557a620
Elana Celano <elanacelano@gmail.com>
2017-09-12 20:47:13 -0400

    first to Heroku

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,12 +3,11 @@ import React from "react";
 class Footer extends React.Component {
 	render() {
 		return (
-			<footer>
 
+			<footer>
 			<div className="footer">
 				&copy; 2017 Elana Celano
 			</div>	
-
 			</footer>	
 		);
 	}
--- src/Components/Pinit.js
+++ src/Components/Pinit.js
@@ -1,11 +1,12 @@
 import React from "react";
 // import { PinItButton, PinterestFollowButton, PinterestPinWidget, PinterestBoardWidget, PinterestProfileWidget, PinterestGrid, PinterestPinWidget } from "react-pinterest";
+import {Container, Row} from "reactstrap";
 import {PinItButton,
     PinterestFollowButton,
     PinterestPinWidget,
     PinterestBoardWidget,
     PinterestProfileWidget,
-    PinterestGrid} from "react-pinterest";
+    PinterestGrid} from "react-pinterest";    
 const size = location.search ? location.search.split('=')[1] : 'small';
 const sizeMap = { small: 237, medium: 345, large: 600 };
 const gutter = 10;
@@ -14,29 +15,34 @@ const gridStyle = { width: width + 'px' };
 export default class pinIt extends React.Component {
   render() {
     return (
-      <div className="pinterest-app">
-                 
-                <PinterestPinWidget size={size} pin="530158187357124374" />
-                <PinterestPinWidget size={size} pin="356417757989339525" />
-                <PinterestPinWidget size={size} pin="356417757986524080" />
-                <PinterestPinWidget size={size} pin="356417757986724718" />
-                <PinterestPinWidget size={size} pin="356417757988564358" />
-                <PinterestPinWidget size={size} pin="356417757988206582" />
-                <PinterestPinWidget size={size} pin="202802789445693269" />
-                <PinterestPinWidget size={size} pin="89438742571585339" />
-                <PinterestPinWidget size={size} pin="232850243203755319" />
-                <PinterestPinWidget size={size} pin="137008013639035297" />
-                <PinterestPinWidget size={size} pin="264797653065817757" />
-                <PinterestPinWidget size={size} pin="144467100519920447" />
+        <div className="pinterest-app">
+            <div className="album text-muted">
+
+            <Row>
+            <div className="col-md-4">
+                <PinterestPinWidget size={size} pin="567735096748540707" />
             </div>
-        );
-      }
-    }
+            <div className="col-md-4">   
+                <PinterestPinWidget size={size} pin="193795590184950367" />
+            </div>
+            <div className="col-md-4">
+                <PinterestPinWidget size={size} pin="487233253427671247" />
+            </div>
+            </Row>
+                </div>
+            </div>
+    
+    );
+  }
+}
 
 
 
 
-
+      // <div className="pinterest-app">
+      //   <div className="pinIt">
+      //       <div className="album-text-muted">
+      //           <div className="container">
 
 
 
