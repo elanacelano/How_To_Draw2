825ace6d3eefc93d2eb82c74f9e96714d6eebb8c
Elana Celano <elanacelano@gmail.com>
2017-09-14 15:12:16 -0400

    artist names

--- src/Components/Pinit.js
+++ src/Components/Pinit.js
@@ -20,30 +20,39 @@ export default class pinIt extends React.Component {
             <Row>
             <div className="col-md-4">
                 <PinterestPinWidget size={size} pin="567735096748540707" />
+                <p>Pablo Picasso</p>
             </div>
             <div className="col-md-4">   
                 <PinterestPinWidget size={size} pin="193795590184950367" />
+                <p>Vincent Van Gough</p>
             </div> 
             <div className="col-md-4">
                 <PinterestPinWidget size={size} pin="215891375860202940" />
+                <p>Albrecht Durer</p>
             </div>
             <div className="col-md-4">
                 <PinterestPinWidget size={size} pin="177118197815718623" />
+                <p>Gustav Klimpt</p>
             </div>
             <div className="col-md-4">
                 <PinterestPinWidget size={size} pin="27373510206669218" />
+                <p>Kathe Kollowitz</p>
             </div>
             <div className="col-md-4">
                 <PinterestPinWidget size={size} pin="588986457488139167" />
+                <p>MC Escher</p>
             </div>
             <div className="col-md-4">
                 <PinterestPinWidget size={size} pin="457678380854076785" />
+                <p>Vincent Van Gogh</p>
             </div>
             <div className="col-md-4">
                 <PinterestPinWidget size={size} pin="244601823481707800" />
+                <p>Edgar Degas</p>
             </div> 
             <div className="col-md-4">
                 <PinterestPinWidget size={size} pin="487233253427671247" />
+                <p>Mucha</p>
             </div>
             </Row>
                 </div>
